5f4a45755ac2ea09cc824268fdc9f762d9f9a112
Rakesh <rakeshr.innovate@gmail.com>
2026-02-03 03:37:38 +0530

    fix: disable SSR for verify and dashboard pages

--- client/src/app/dashboard/faculty/classes/page.tsx
+++ client/src/app/dashboard/faculty/classes/page.tsx
@@ -1,4 +1,5 @@
-'use client';
+"use client";
+export const dynamic = "force-dynamic";
 
 import MentorDashboardLayout from '@/components/MentorDashboardLayout';
 import HodDashboardLayout from '@/components/HodDashboardLayout';
